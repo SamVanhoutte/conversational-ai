366e65a5faee3a13b2980b77475f01f6a631f33e
Sam <sam.vanhoutte@marchitec.be>
2021-11-03 09:54:42 +0100

    initial demo for nltk

---
+++ src/01 nltk/01-nltk-demo.py
@@ -0,0 +1,8 @@
+import nltk
+nltk.download('averaged_perceptron_tagger')
+sentence = "I want to book a flight for 4 persons from New York to Amsterdam"
+tokens =nltk.word_tokenize(sentence)
+print(tokens)
+tagged_tokens = nltk.pos_tag(tokens)
+for tagged_token in tagged_tokens:
+    print(tagged_token[0].rjust(15), tagged_token[1])
